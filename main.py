a37a4066ce14f5d1a9b2726af8f77c317e5180f4
NatalieR2026 <natalie.radin2026@gbwl.org>
2026-01-16 14:41:05 +0000

    1/16

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
 
+plt.style.use('seaborn-v0_8-deep')
+
+df = pd.read_csv('shopping_behavior_updated.csv')
+df_clothes = df[df['Category'] == 'Clothing']
 
