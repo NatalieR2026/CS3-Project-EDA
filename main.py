0150342dc9e4526c6dee583cc7afc27589bf5669
NatalieR2026 <natalie.radin2026@gbwl.org>
2026-01-23 14:40:26 +0000

    1/23

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 
-plt.style.use('seaborn-v0_8-deep')
+plt.style.use('seaborn-v0_8-ticks')
+# print(plt.style.available)
 
 # Graph 1: Is there a correlation between seasons and clothing item purchased?
 
 df = pd.read_csv('shopping_behavior_updated.csv')
 df_clothes = df[df['Category'] == 'Clothing']
 
-count_of_items = df_clothes.count('Clothing')
-print(count_of_items)
+pivot_df = df_clothes.pivot_table(index='Season', columns='Item Purchased', values='Customer ID', aggfunc='count', fill_value=0)
 
-clothing = sns.catplot(x='Season', y=sequence.count('Item Purchased'), color='Item Purchased', kind='bar', stacked=True)
+pivot_df.plot(kind='bar', stacked=True, figsize=(10,6))
+plt.title('Clothing Items by Season')
+plt.xlabel('Season')
+plt.ylabel('Amount')
+plt.legend(title='Item Purchased')
+plt.savefig('stacked_bar_clothes.png', bbox_inches='tight')
 
-plt.savefig('clothes_season_stackedbarplot.png')
+# Graph 2: more specific
+
